651bb7802e5fcb1f6dbfb8852dce707fbff6b6d2
Ahsan-code <ahsanhabib33037768@gmail.com>
2023-03-25 16:45:28 +0600

    built home page and office page

--- myapp/views.py
+++ myapp/views.py
@@ -6,7 +6,7 @@ def index(request):
     context = {'name':'ahsan','types':'student'}
     return render(request, 'index.html',context)
 def home(request):
-    return HttpResponse('this is home section')
+    return render(request,'home.html')
 
 def office(request):
-    return HttpResponse('This page is dedicated to official matterials')
+    return render(request,'office.html')
