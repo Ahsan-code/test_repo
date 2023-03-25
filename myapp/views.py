cce94112657ebf391d4f40df24704541a5de4a80
Ahsan-code <ahsanhabib33037768@gmail.com>
2023-03-25 10:06:58 +0600

    context added along with html file rendering

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):   
-    return render(request, 'index.html')
+    context = {'name':'ahsan','types':'student'}
+    return render(request, 'index.html',context)
 def home(request):
     return HttpResponse('this is home section')
 
